f56b5e5571ef3f75488ab119243d0a997d99bbba
Joey Wunderlich <jwunderl@users.noreply.github.com>
2021-03-09 16:06:34 -0800

    add function in test to generate icon

--- test.ts
+++ test.ts
@@ -1,70 +1,140 @@
 
-game.consoleOverlay.setVisible(true, 2)
-tiles.setTilemap(tilemap`level`)
+createIcon()
+function createIcon() {
+    scene.setBackgroundColor(13)
+    tiles.setTilemap(tilemap`level1`);
+    const start = tiles.getRandomTileByType(myTiles.tile2);
+    const end = tiles.getRandomTileByType(myTiles.tile3)
+    const path = scene.aStar(start, end)
 
-let mySprite: Sprite = sprites.create(img`
-    . . . . . f f 4 4 f f . . . . .
-    . . . . f 5 4 5 5 4 5 f . . . .
-    . . . f e 4 5 5 5 5 4 e f . . .
-    . . f b 3 e 4 4 4 4 e 3 b f . .
-    . . f 3 3 3 3 3 3 3 3 3 3 f . .
-    . f 3 3 e b 3 e e 3 b e 3 3 f .
-    . f 3 3 f f e e e e f f 3 3 f .
-    . f b b f b f e e f b f b b f .
-    . f b b e 1 f 4 4 f 1 e b b f .
-    f f b b f 4 4 4 4 4 4 f b b f f
-    f b b f f f e e e e f f f b b f
-    . f e e f b d d d d b f e e f .
-    . . e 4 c d d d d d d c 4 e . .
-    . . e f b d b d b d b b f e . .
-    . . . f f 1 d 1 d 1 d f f . . .
-    . . . . . f f b b f f . . . . .
-`)
-controller.moveSprite(mySprite)
-scene.cameraFollowSprite(mySprite)
-
-let flag = false;
-controller.A.onEvent(ControllerButtonEvent.Pressed, () => {
-    const res = scene.aStar(tiles.getTileLocation(0, 11), tiles.getTileLocation(0, 3), sprites.castle.tilePath5);
-    const enemy = sprites.create(img`
-        . . . . . . . . . . . . . . . .
-        . . . . . . 6 6 6 6 . . . . . .
-        . . . . 6 6 6 5 5 6 6 6 . . . .
-        . . . 7 7 7 7 6 6 6 6 6 6 . . .
-        . . 6 7 7 7 7 8 8 8 1 1 6 6 . .
-        . . 7 7 7 7 7 8 8 8 1 1 5 6 . .
-        . 6 7 7 7 7 8 8 8 8 8 5 5 6 6 .
-        . 6 7 7 7 8 8 8 6 6 6 6 5 6 6 .
-        . 6 6 7 7 8 8 6 6 6 6 6 6 6 6 .
-        . 6 8 7 7 8 8 6 6 6 6 6 6 6 6 .
-        . . 6 8 7 7 8 6 6 6 6 6 8 6 . .
-        . . 6 8 8 7 8 8 6 6 6 8 6 6 . .
-        . . . 6 8 8 8 8 8 8 8 8 6 . . .
-        . . . . 6 6 8 8 8 8 6 6 . . . .
-        . . . . . . 6 6 6 6 . . . . . .
-        . . . . . . . . . . . . . . . .
-    `, SpriteKind.Enemy)
-    enemy.setPosition(mySprite.x, mySprite.y)
-    scene.followPath(enemy, res)
-});
-mySprite.setFlag(SpriteFlag.BounceOnWall, false)
-scene.onPathCompletion(SpriteKind.Enemy, function (sprite: Sprite, location: tiles.Location) {
-    sprite.destroy(effects.ashes);
-});
-
-controller.B.onEvent(ControllerButtonEvent.Pressed, () => {
-    const pfs = sprites.allOfKind(SpriteKind.Enemy);
-    pfs.forEach(s => {
-        scene.followPath(s, null);
-        s.vx = 0;
-        s.vy = 0;
+    game.onUpdateInterval(1500, function() {
+        const car = sprites.create(img`
+            . . . . . . . . . . . . . . . .
+            . . . . 6 6 6 6 6 6 6 6 . . . .
+            . . . 6 9 6 6 6 6 6 6 c 6 . . .
+            . . 6 c 9 6 6 6 6 6 6 c c 6 . .
+            . 6 c c 9 9 9 9 9 9 6 c c 9 6 d
+            . 6 c 6 8 8 8 8 8 8 8 b c 9 6 6
+            . 6 6 8 b b 8 b b b 8 8 b 9 6 6
+            . 6 8 b b b 8 b b b b 8 6 6 6 6
+            . 8 8 6 6 6 8 6 6 6 6 6 8 6 6 6
+            . 8 8 8 8 8 8 f 8 8 8 f 8 6 d d
+            . 8 8 8 8 8 8 f 8 8 f 8 8 8 6 d
+            . 8 8 8 8 8 8 f f f 8 8 8 8 8 8
+            . 8 f f f f 8 8 8 8 f f f 8 8 8
+            . . f f f f f 8 8 f f f f f 8 .
+            . . . f f f . . . . f f f f . .
+            . . . . . . . . . . . . . . . .
+        `, randint(1, 3))
+        tiles.placeOnTile(car, start)
+        scene.followPath(car, path)
     });
-});
 
-const dbg = true;
-game.onUpdate(() => {
-    if (dbg) {
-        sprites.allOfKind(SpriteKind.Enemy)
-            .forEach(s => s.say(Math.round(scene.spritePercentPathCompleted(s)).toString()));
+    game.onUpdate(() => {
+        for (const s of sprites.allOfKind(1)) {
+            if (s.vy < 0) {
+                s.setImage(sprites.vehicle.carBlueBack)
+            } else if (s.vx > 0) {
+                s.setImage(sprites.vehicle.carBlueRight)
+            } else if (s.vx < 0) {
+                s.setImage(sprites.vehicle.carBlueLeft)
+            }
+        }
+        for (const s of sprites.allOfKind(2)) {
+            if (s.vy < 0) {
+                s.setImage(sprites.vehicle.carRedBack)
+            } else if (s.vx > 0) {
+                s.setImage(sprites.vehicle.carRedRight)
+            } else if (s.vx < 0) {
+                s.setImage(sprites.vehicle.carRedLeft)
+            }
+        }
+        for (const s of sprites.allOfKind(3)) {
+            if (s.vy < 0) {
+                s.setImage(sprites.vehicle.carPinkBack)
+            } else if (s.vx > 0) {
+                s.setImage(sprites.vehicle.carPinkRight)
+            } else if (s.vx < 0) {
+                s.setImage(sprites.vehicle.carPinkLeft)
+            }
+        }
+    });
+    for (let i = 1; i <= 3; ++i ){
+        scene.onPathCompletion(
+            i,
+            (sprite: Sprite, location: tiles.Location) => sprite.destroy()
+        );
     }
-});
+}
+
+function testPath() {
+    game.consoleOverlay.setVisible(true, 2)
+    tiles.setTilemap(tilemap`level`)
+
+    let mySprite: Sprite = sprites.create(img`
+        . . . . . f f 4 4 f f . . . . .
+        . . . . f 5 4 5 5 4 5 f . . . .
+        . . . f e 4 5 5 5 5 4 e f . . .
+        . . f b 3 e 4 4 4 4 e 3 b f . .
+        . . f 3 3 3 3 3 3 3 3 3 3 f . .
+        . f 3 3 e b 3 e e 3 b e 3 3 f .
+        . f 3 3 f f e e e e f f 3 3 f .
+        . f b b f b f e e f b f b b f .
+        . f b b e 1 f 4 4 f 1 e b b f .
+        f f b b f 4 4 4 4 4 4 f b b f f
+        f b b f f f e e e e f f f b b f
+        . f e e f b d d d d b f e e f .
+        . . e 4 c d d d d d d c 4 e . .
+        . . e f b d b d b d b b f e . .
+        . . . f f 1 d 1 d 1 d f f . . .
+        . . . . . f f b b f f . . . . .
+    `)
+    controller.moveSprite(mySprite)
+    scene.cameraFollowSprite(mySprite)
+
+    let flag = false;
+    controller.A.onEvent(ControllerButtonEvent.Pressed, () => {
+        const res = scene.aStar(tiles.getTileLocation(0, 11), tiles.getTileLocation(0, 3), sprites.castle.tilePath5);
+        const enemy = sprites.create(img`
+            . . . . . . . . . . . . . . . .
+            . . . . . . 6 6 6 6 . . . . . .
+            . . . . 6 6 6 5 5 6 6 6 . . . .
+            . . . 7 7 7 7 6 6 6 6 6 6 . . .
+            . . 6 7 7 7 7 8 8 8 1 1 6 6 . .
+            . . 7 7 7 7 7 8 8 8 1 1 5 6 . .
+            . 6 7 7 7 7 8 8 8 8 8 5 5 6 6 .
+            . 6 7 7 7 8 8 8 6 6 6 6 5 6 6 .
+            . 6 6 7 7 8 8 6 6 6 6 6 6 6 6 .
+            . 6 8 7 7 8 8 6 6 6 6 6 6 6 6 .
+            . . 6 8 7 7 8 6 6 6 6 6 8 6 . .
+            . . 6 8 8 7 8 8 6 6 6 8 6 6 . .
+            . . . 6 8 8 8 8 8 8 8 8 6 . . .
+            . . . . 6 6 8 8 8 8 6 6 . . . .
+            . . . . . . 6 6 6 6 . . . . . .
+            . . . . . . . . . . . . . . . .
+        `, SpriteKind.Enemy)
+        enemy.setPosition(mySprite.x, mySprite.y)
+        scene.followPath(enemy, res)
+    });
+    mySprite.setFlag(SpriteFlag.BounceOnWall, false)
+    scene.onPathCompletion(SpriteKind.Enemy, function (sprite: Sprite, location: tiles.Location) {
+        sprite.destroy(effects.ashes);
+    });
+
+    controller.B.onEvent(ControllerButtonEvent.Pressed, () => {
+        const pfs = sprites.allOfKind(SpriteKind.Enemy);
+        pfs.forEach(s => {
+            scene.followPath(s, null);
+            s.vx = 0;
+            s.vy = 0;
+        });
+    });
+
+    const dbg = false;
+    game.onUpdate(() => {
+        if (dbg) {
+            sprites.allOfKind(SpriteKind.Enemy)
+                .forEach(s => s.say(Math.round(scene.spritePercentPathCompleted(s)).toString()));
+        }
+    });
+}
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -4,10 +4,17 @@ namespace myTiles {
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
 
-    helpers.registerTilemapFactory(function(name: string) {
+    helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "level": return tiles.createTilemap(hex`1000100002020202020202020202020202020202020202020202020202020202020202020202020201020202020202020202020201010101010101010101010202020202020202020202020202020101020202020202020202020201020202010102020202020202020202010202020201020202020202020202020102020202010202020202020101010101020202010102020202020202020102020202010102020202020202020201020202010102020202020101010101010101010102020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202`, img`
+            case "level":
+            case "level":return tiles.createTilemap(hex`1000100002020202020202020202020202020202020202020202020202020202020202020202020201020202020202020202020201010101010101010101010202020202020202020202020202020101020202020202020202020201020202010102020202020202020202010202020201020202020202020202020102020202010202020202020101010101020202010102020202020202020102020202010102020202020202020201020202010102020202020101010101010101010102020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -24,7 +31,32 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.castle.tilePath5,sprites.castle.tileGrass1], TileScale.Sixteen)
+`, [myTiles.transparency16,sprites.castle.tilePath5,sprites.castle.tileGrass1], TileScale.Sixteen);
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`0a0008000807070707070707070704070707070707070707010202020205070707070707070707040707070707070707070102020205070707070707070707040602020202020202020307070707070707070707`, img`
+. 2 2 2 2 2 2 2 2 2 
+. 2 2 2 2 2 2 2 2 2 
+. . . . . . 2 2 2 2 
+2 2 2 2 2 . 2 2 2 2 
+2 2 2 2 2 . . . . . 
+2 2 2 2 2 2 2 2 2 . 
+. . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.vehicle.roadTurn3,sprites.vehicle.roadHorizontal,sprites.vehicle.roadTurn4,sprites.vehicle.roadVertical,sprites.vehicle.roadTurn2,myTiles.tile2,sprites.castle.tilePath5,myTiles.tile3], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "tile1":return tile1;
+            case "myTile":
+            case "tile2":return tile2;
+            case "myTile0":
+            case "tile3":return tile3;
+            case "myTile1":
+            case "tile4":return tile4;
         }
         return null;
     })
